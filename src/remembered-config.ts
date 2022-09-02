04dfc462d1e51474951d6c8824258da26c86cdb1
Thiago Santos <tos.oliveira@gmail.com>
2022-09-02 09:49:51 -0300

    feat: version bump

--- src/remembered-config.ts
+++ src/remembered-config.ts
@@ -3,7 +3,7 @@ export type Ttl = number | (<T>(request: T) => number);
 export interface RememberedConfig {
 	ttl: Ttl;
 	/**
-	 * Always keep a persistent last result for the cache when there is one, so the cache can be updated in background
+	 * Always keep a persistent last result for the cache when there is one, so the cache can be updated in the background
 	 */
 	nonBlocking?: boolean;
 	onReused?: (key: string) => void;
