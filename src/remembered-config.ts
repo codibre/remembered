7821cea5a5f9c6deddcc236b49ac40497cb16618
IcaroLettieri <icaro_lettieri@live.com>
2023-08-30 14:33:54 -0300

    feat: implement getTll from ttlFunction in constructor configuration

--- src/remembered-config.ts
+++ src/remembered-config.ts
@@ -1,7 +1,14 @@
-export type Ttl = number | (<T>(request: T) => number);
+export type TtlFunction<TResponse = unknown, TKey = string> = (
+	key: TKey,
+	response?: TResponse,
+) => number;
 
-export interface RememberedConfig {
-	ttl: Ttl;
+export type Ttl<TResponse = unknown, TKey = string> =
+	| number
+	| TtlFunction<TResponse, TKey>;
+
+export interface RememberedConfig<TResponse = unknown, TKey = string> {
+	ttl: Ttl<TResponse, TKey>;
 	/**
 	 * Always keep a persistent last result for the cache when there is one, so the cache can be updated in the background
 	 */
